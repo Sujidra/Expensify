ba7c1beb11622f92cfc327bf8fe1a653bb5a72ed
Sujidra <sujidrarathinavel@gmail.com>
2020-05-07 21:41:13 +0530

    after rectifying

--- src/routers/components/store/selectors/getTotalExpense.js
+++ src/routers/components/store/selectors/getTotalExpense.js
@@ -1,7 +1,7 @@
 const getTotalExpense=(expenses)=>{
     let i,total=0;
     for(i=0;i<expenses.length;i++){
-        total=total+expenses[0].amt;
+        total=total+expenses[i].amt;
         //total=expenses[1].amount+total;
     }
     return total;
